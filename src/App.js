4c3f209e273df788bb073eaecc40750a4a403325
Auezkhan Daniyar <nurovich14@gmail.com>
2021-01-25 17:11:30 +0600

    testing

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () =>
     return(
         <Container maxsidth='lg'>
             <AppBar className={classes.appBar} position='static' color='inherit'>
-                <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
+                <Typography className={classes.heading} variant='h2' align='center'>Random Posts</Typography>
                 <img className={classes.image} src={memories} alt='memories' height="60" />
             </AppBar>
             <Grow in>
